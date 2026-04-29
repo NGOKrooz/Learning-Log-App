20e4cc0a02392db1296bdb2fe65515b03d7124b1
TOPboy <godswillnnachi5@gmail.com>
2026-04-29 15:16:31 +0100

    Redesign navbar to simple fixed top bar and clean mobile nav

--- static/js/main.js
+++ static/js/main.js
@@ -14,7 +14,6 @@ class LearningLogUI {
     this.setupPasswordRequirements();
     this.setupAlerts();
     this.setupTooltips();
-    this.setupNavigation();
   }
 
   /**
@@ -162,23 +161,6 @@ class LearningLogUI {
           }
         }, 5000);
       }
-    });
-  }
-
-  /**
-   * Setup navigation menu toggling
-   */
-  setupNavigation() {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-
-    if (!navToggle || !navMenu) {
-      return;
-    }
-
-    navToggle.addEventListener('click', () => {
-      const isOpen = navMenu.classList.toggle('open');
-      navToggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
     });
   }
 
